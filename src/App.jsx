9e5d24492571f0e9ce8fd500246252036d7dd6a2
solunska <a_solunska@outlook.com>
2024-04-25 19:48:43 +0200

    minor changes to the style

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,76 @@
-import {useState} from 'react';
+import { useState } from 'react';
 import './components/Navbar/Navbar.css';
 import './App.css';
 import Navbar from "./components/Navbar/Navbar.jsx";
 import AboutMe from './components/AboutMe/AboutMe.jsx';
+import Resume from './components/Resume/Resume.jsx';
 
 function App() {
+    const [buttonStyles, setButtonStyles] = useState({
+        aboutMe: 'button-active',
+        resume: 'button-unactive',
+        projects: 'button-unactive',
+        contact: 'button-unactive',
+    })
+
+
+    function handleAboutMeClick() {
+        setButtonStyles({
+            aboutMe: 'button-active',
+            resume: 'button-unactive',
+            projects: 'button-unactive',
+            contact: 'button-unactive',
+        })
+    }
+
+    function handleResumeClick() {
+        setButtonStyles({
+            aboutMe: 'button-unactive',
+            resume: 'button-active',
+            projects: 'button-unactive',
+            contact: 'button-unactive',
+        })
+    }
+
+    function handleProjectsClick() {
+        setButtonStyles({
+            aboutMe: 'button-unactive',
+            resume: 'button-unactive',
+            projects: 'button-active',
+            contact: 'button-unactive',
+        })
+    }
+
+    function handleContactClick() {
+        setButtonStyles({
+            aboutMe: 'button-unactive',
+            resume: 'button-unactive',
+            projects: 'button-unactive',
+            contact: 'button-active',
+        })
+    }
+
+    let content = '';
+
+    if (buttonStyles.aboutMe === 'button-active') {
+        content = <AboutMe />;
+    } else if (buttonStyles.resume === 'button-active') {
+        content = <Resume />
+    } else if (buttonStyles.projects === 'button-active') {
+        content = <AboutMe />;
+    } else if (buttonStyles.contact === 'button-active') {
+        content = <AboutMe />;
+    }
 
     return (
         <>
-            <Navbar/>
-            <AboutMe/>
+            <Navbar
+                buttonStyles={buttonStyles}
+                aboutMeClick={handleAboutMeClick}
+                projectsClick={handleProjectsClick}
+                contactClick={handleContactClick}
+                resumeClick={handleResumeClick} />
+            {content}
         </>
     )
 }
