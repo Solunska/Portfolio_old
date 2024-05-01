43fb737f9b5b1e8c5a6f58225da4650f30b2e12d
solunska <a_solunska@outlook.com>
2024-05-01 07:04:21 +0200

    minor changes to style

--- src/components/AboutMe/AboutMeSidebar/AboutMeSidebar.jsx
+++ src/components/AboutMe/AboutMeSidebar/AboutMeSidebar.jsx
@@ -5,12 +5,14 @@ export default function AboutMeSidebar() {
     return (
         <div className="about-me-side-bar-container">
             {/* <img src={profilePhoto} alt="Profile Photo" /> */}
-            <div className='profile-photo-container'>
-                <div className='profile-photo'></div>
-            </div>
-            <div className='info-container'>
-                <Info text='UX/UI Designer and Developer' />
-                <Info text='Studying at Faculty of Computer Science and Engineering ' />
+            <div className='container'>
+                <div className='profile-photo-container'>
+                    <div className='profile-photo'></div>
+                </div>
+                <div className='info-container'>
+                    <Info text='UX/UI Designer and Developer' />
+                    <Info text='Studying at Faculty of Computer Science and Engineering ' />
+                </div>
             </div>
         </div>
     );
